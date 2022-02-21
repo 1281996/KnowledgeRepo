b7477c3e35069d3899e7c1ae89bcb17aaf1c448b
mallika <mallika.kamma@gmail.com>
2022-02-21 15:36:41 +0530

    3rd merge

--- Quikr-Project/src/main/java/cap/cg/pojo/Student.java
+++ Quikr-Project/src/main/java/cap/cg/pojo/Student.java
@@ -1,16 +1,24 @@
 package cap.cg.pojo;
 
 public class Student {
-void m1() {
-	System.out.println("Student");
-}
-void m2() {
-	System.out.println("Student");
-}
-void m3() {
-System.out.println("m3 student");	
-}
-void m4() {
-System.out.println("m3 student");	
-}
+	void m1() {
+		System.out.println("Student");
+	}
+
+	void m2() {
+		System.out.println("Student");
+	}
+
+	void m3() {
+		System.out.println("m3 student");
+	}
+
+	void m4() {
+		System.out.println("m3 student");
+	}
+
+	void m5() {
+		System.out.println("m5 student");
+	}
+
 }
